4302f6487e1e589e369fd3635cca97047b546664
daraksha-dtg <daraksha.atg@gmail.com>
2022-01-09 22:31:49 +0530

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Header.scss";
 import logo from "../../assets/logo.png";
+import location from "../../assets/location.png";
+import Arrow from "../../assets/arrow.svg";
+import Search from "../../assets/search.svg";
 
 const Header = () => {
   return (
@@ -11,6 +14,34 @@ const Header = () => {
       <h2 className="header-text">
         Discover the best food & drinks in Delhi NCR
       </h2>
+      <div className="todo">
+        <div className="header-left">
+          <div>
+            <img src={location} alt="location" className="location" />
+          </div>
+          <div>
+            <input
+              value=""
+              placeholder="Ywca, 1, Ashoka Rd, Hanuman Road Area, Connaught Place, New Delhi, Delhi"
+              class="address"
+            />
+          </div>
+          <div>
+            <img src={Arrow} alt="arrow" className="arrow" />
+          </div>
+        </div>
+        <div className="line"></div>
+        <div className="search">
+          <img src={Search} alt="search" className="search-icon" />
+        </div>
+        <div className="header-right">
+          <input
+            value=""
+            placeholder="Search for restaurant, cuisine or a dish"
+            class="right"
+          />
+        </div>
+      </div>
     </div>
   );
 };
