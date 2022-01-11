4c0c11bbfa4ff72169d5ac709d12f3bed51d3b6b
daraksha-dtg <daraksha.atg@gmail.com>
2022-01-11 21:49:09 +0530

    updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -111,15 +111,15 @@ const Footer = () => {
           </div>
           <div className="app">
             <img
-              alt="image"
-              className="image"
+              alt="app"
+              className="app-logo"
               src="https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png"
             />
           </div>
           <div>
             <img
-              alt="image"
-              className="image"
+              alt="app"
+              className="app-logo"
               src="https://b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png"
             />
           </div>
